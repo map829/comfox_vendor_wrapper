e73eddca813e8e237a9987f1c9f4d752732667e1
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-16 23:24:24 +0100

    Renamed due to clean coding naming convention

---
+++ credentials_reader/JSONCredentialFileReader.py
@@ -0,0 +1,9 @@
+from credentials_reader.CredentialFileReader import CredentialFileReader
+
+
+class JSONCredentialFileReader(CredentialFileReader):
+    def get_credentials(self, config_file_name=None):
+        pass
+
+    def __init__(self):
+        pass
